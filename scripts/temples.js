4d810d4ff91ad099bc00f1de21fe4a56986cabfa
GustavoColman <luizgustavocolman700@gmail.com>
2026-03-23 18:11:55 -0300

    semana 2

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,17 @@ ano.innerHTML = today.getFullYear();
 //  Pega a data da última modificação do documento
 const lastModifiedElement = document.querySelector("#lastModified");
 lastModifiedElement.innerHTML = `Last Modification: ${document.lastModified}`;
+
+
+// Seleciona o botão e a lista de navegação
+const menuButton = document.querySelector('.menu-button');
+const navigation = document.querySelector('.navigation');
+
+// Adiciona um evento de clique
+menuButton.addEventListener('click', () => {
+    // Alterna a classe "show" que criamos no CSS
+    navigation.classList.toggle('show');
+    
+    // Opcional: muda o visual do botão quando aberto
+    menuButton.classList.toggle('open');
+});
